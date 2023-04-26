98b69d1829b1058c8f1021b86a75a1c4ff3869c9
chemsdine777 <104915350+chemsdine777@users.noreply.github.com>
2023-04-26 16:02:42 +0200

    structuration

--- Application/Home.py
+++ Application/Home.py
@@ -2,13 +2,20 @@
 import streamlit as st 
 from bdd import create_table
 
+from modelling import print_picture, x_test
+
 def main():
-    # Création de la table
-    create_table(table_name="picture_predict")
     
-    # Accueil (titre & header)
+    # Création de la table.
+    create_table(table_name="picture_predict", name_bdd="neuronal_convolutif")
+    
+    # Accueil (titre & header).
     st.header("Outil de visualisation pour un réseau neuronal convolutif")
-    st.title("Accueil")     
+    st.title("Accueil")  
+    
+    # Affichage d'une image.
+    print_picture()
+       
 main()
 
 
--- Application/bdd.py
+++ Application/bdd.py
@@ -7,8 +7,11 @@ import numpy as np
 import pymysql
 import pandas as pd
 
-def create_table(table_name:str):  
-    engine=create_engine('mysql+pymysql://root:@localhost/neuronal_convolutif')
+# Fonction permettent de créer une table.
+def create_table(table_name:str, name_bdd:str):  
+    
+    # connexion à la base de données.
+    engine=create_engine(f'mysql+pymysql://root:@localhost/{name_bdd}')
     inspector=inspect(engine)
     
     # Si la table n'existe pas on la créer.
@@ -18,10 +21,12 @@ def create_table(table_name:str):
         # Typage des colonnes de la Table SQL.
         for i in df:
             df[i]=df[i].astype('int64')
-        # Insérer la Target en première position
+        # Insérer la Target en première position.
         label = ''
         df.insert(loc=0, column='label', value=label)
         # envoie du DataFrame sur SQL.
         df.to_sql(name=table_name, con=engine, if_exists='fail', index=False)
+    
+    # Message pour l'utilisateur. 
     print(f"Création de la table {table_name} avec succès.")
 

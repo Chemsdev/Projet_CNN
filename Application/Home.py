b87a1107ec63a3d5489141b5dd65c148dfe6e395
chemsdine777 <104915350+chemsdine777@users.noreply.github.com>
2023-04-27 09:34:56 +0200

    initialisation du projet

--- Application/Home.py
+++ Application/Home.py
@@ -2,8 +2,6 @@
 import streamlit as st 
 from bdd import create_table
 
-from modelling import print_picture, x_test
-
 def main():
     
     # Création de la table.
@@ -12,10 +10,7 @@ def main():
     # Accueil (titre & header).
     st.header("Outil de visualisation pour un réseau neuronal convolutif")
     st.title("Accueil")  
-    
-    # Affichage d'une image.
-    print_picture()
-       
+
 main()
 
 
